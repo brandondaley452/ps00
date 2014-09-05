25d3be97a6168b4bb201911c51e10f3f39cf7f3f
Brandon Daley <bdaley@ccs.neu.edu>
2014-09-05 14:22:06 -0400

    Update 12.rkt
    Answered Ex 12

--- set00/12.rkt
+++ set00/12.rkt
@@ -2,11 +2,12 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |12|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ; Ex 12
-; function-name : DataType DataType -> DataType
-; GIVEN: 
-; RETURNS: 
-; Examples:
-; (function-name) => 
-; (function-name) => 
-; (function-name) => 
-; (function-name) => 
+;
+; Predictions for function calls:
+; 
+; (make-point 5 3)             -> (make-point 5 3) -> CORRECT PREDICTION
+; (point? 5)                   -> false            -> CORRECT PREDICTION
+; (point? true)                -> false            -> CORRECT PREDICTION
+; (point? (make-point 2 1))    -> true             -> CORRECT PREDICTION
+; (point-x (make-point 8 5))   -> 8                -> CORRECT PREDICTION
+; (point-y (make-point 42 15)) -> 15               -> CORRECT PREDICTION
