fa46937bfa1efdc8f67c679f1151102a853fe8a8
Brandon Daley <bdaley@ccs.neu.edu>
2014-09-05 11:00:10 -0400

    Update 8.rkt
    Added error checking and test cases

--- set00/8.rkt
+++ set00/8.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |8|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ; Ex 8
 ; circle-area : Number -> Number
 ; GIVEN: the radius r of a circle 
@@ -9,6 +6,28 @@
 ; (circle-area 1) => 3.141592653589793
 ; (circle-area 5) => 78.53981633974483
 ; (circle-area 7) => 153.93804002589985
-(define (circle-area Radius)
-  (* pi (sqr Radius))
+(define (circle-area radius)
+  (if (>= radius 0)
+      (* pi (sqr radius))
+      "The radius of the circle must be at least zero."
+      )
   )
+
+; Tests for circle-area
+(check-within
+ (circle-area 1)
+ 3.141592653589793
+ 0.0000000001
+ )
+
+(check-within
+ (circle-area 5)
+ 78.53981633974483
+ 0.0000000001
+ )
+
+(check-within
+ (circle-area 7)
+ 153.93804002589985
+ 0.0000000001
+ )
