050e8e4d39f550791c8077062524b3b47660dc11
Brandon Daley <bdaley@ccs.neu.edu>
2014-09-05 09:00:24 -0400

    Update 3.rkt
    Now contains tests

--- set00/3.rkt
+++ set00/3.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |3|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ; Ex 3
 ; f->c : Number -> Number
 ; GIVEN: a temperature in degrees Fahrenheit as an argument
@@ -10,4 +7,16 @@
 ; (f->c 100) => 37.77777777777778
 (define (f->c FTemp)
     (* 5/9 (- FTemp 32))
+  )
+
+; Tests for f->c
+(check-expect
+  (f->c 32)
+  0
+  )
+
+(check-within
+  (f->c 100)
+  37.7777
+  0.0001
   )
