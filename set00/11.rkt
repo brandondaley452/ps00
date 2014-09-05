c07e5d50555c1f20697b04f6580a080fd4588e1f
Brandon Daley <bdaley@ccs.neu.edu>
2014-09-05 14:14:55 -0400

    Update 11.rkt

--- set00/11.rkt
+++ set00/11.rkt
@@ -2,11 +2,21 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |11|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ; Ex 11
-; function-name : DataType DataType -> DataType
-; GIVEN: 
-; RETURNS: 
-; Examples:
-; (function-name) => 
-; (function-name) => 
-; (function-name) => 
-; (function-name) => 
+; 
+; Because point-functions represent a point on an (x,y) plane, I would expect the contracts to look something like this:
+;
+; make-point : Real Real -> Point
+; point?     : Point     -> Boolean
+; point-x    : Point     -> Real
+; point-y    : Point     -> Real
+;
+; After examining the posn-functions in Help Desk, the contracts for posn-functions are:
+;
+; make-posn : Any Any -> Point
+; posn?     : Point     -> Boolean
+; posn-x    : Point     -> Any
+; posn-y    : Point     -> Any
+;
+; The big difference between what I predicted and what the posn-functions represent is that I anticipated
+; it to represent data on a standard (x, y) coordinate system from negative infinity to positive infinity
+; on both axes. Instead, posn can hold literally any type of data in each argument.
