4892f338dc46444d7c3f4667b2f782af6eacaa5b
Brandon Daley <bdaley@ccs.neu.edu>
2014-09-05 10:53:58 -0400

    Update 7.rkt
    Added error checking and tests

--- set00/7.rkt
+++ set00/7.rkt
@@ -1,13 +1,32 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |7|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ; Ex 7
 ; circumference : Number -> Number
 ; GIVEN: the radius r of a circle 
 ; RETURNS: its circumference, using the formula 2 * pi * r.
 ; Examples:
-; (circumference 1) => 6.283185307179586 
+; (circumference 1) => 6.283185307179586
 ; (circumference 0) => 0
-(define (circumference Radius)
-  (* 2 pi Radius)
+; (circumference 4) => 25.132741228718345
+(define (circumference radius)
+  (if (>= radius 0)
+      (* 2 pi radius)
+      "The radius of a circle must be at least zero."
+      )
   )
+
+; Tests for circumference
+(check-within
+ (circumference 1)
+ 6.283185
+ 0.0001
+ )
+
+(check-expect
+ (circumference 0)
+ 0
+ )
+
+(check-within
+ (circumference 4)
+ 25.13274
+ 0.0001
+ )
