7885abd3104b4bd4ecfda01d09dc98aafc70a201
Brandon Daley <bdaley@ccs.neu.edu>
2014-09-05 11:19:01 -0400

    Update 10.rkt
    Added tests and error checking

--- set00/10.rkt
+++ set00/10.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |10|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ; Ex 10
 ; two-largest-sum : Number Number Number -> Number
 ; GIVEN: three numbers in any order
@@ -11,17 +8,40 @@
 ; (two-largest-sum 0 0 0)    => 0
 ; (two-largest-sum 100 2 99) => 199
 (define (two-largest-sum a b c)
-  (cond
-    ; check if a is greater than b
-    [(> a b)
-     (cond
-       [(> b c) (+ a b)]
-       [else (+ a c)]
-       )
-     ]
-     ; check if a is greater than c--if it got here, then c is smallest
-     [(> a c) (+ a b)]
-     ; a must be smaller than both so add b and c
-     [else (+ b c)]
-     )
+  (if (and (number? a) (number? b) (number? c))
+      (cond
+        ; check if a is greater than b
+        [(> a b)
+         (cond
+           [(> b c) (+ a b)]
+           [else (+ a c)]
+           )
+         ]
+        ; check if a is greater than c--if it got here, then c is smallest
+        [(> a c) (+ a b)]
+        ; a must be smaller than both so add b and c
+        [else (+ b c)]
+        )
+      "All three arguments provided must be numbers."
+      )
   )
+
+(check-expect
+ (two-largest-sum 1 2 3)
+ 5
+ )
+
+(check-expect
+ (two-largest-sum -8 7 -3)
+ 4
+ )
+
+(check-expect
+ (two-largest-sum 0 0 0)
+ 0
+ )
+
+(check-expect
+ (two-largest-sum 100 2 99)
+ 199
+ )
