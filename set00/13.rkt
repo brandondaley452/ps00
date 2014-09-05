604566faa836318c83f7ba662c5fee54e4d777f2
Brandon Daley <bdaley@ccs.neu.edu>
2014-09-05 14:26:47 -0400

    Update 13.rkt
    Completed ex 13.

--- set00/13.rkt
+++ set00/13.rkt
@@ -2,11 +2,9 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |13|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ; Ex 13
-; function-name : DataType DataType -> DataType
-; GIVEN: 
-; RETURNS: 
-; Examples:
-; (function-name) => 
-; (function-name) => 
-; (function-name) => 
-; (function-name) => 
+;
+; (make-point true false) will return (make-point true false) as a visual representation
+; The call doesn't actually create a point because we have not assigned it to a variable.
+;
+; (point-x (make-point true false)) returns true, as expected.
+; make-point assigns true to x and false to y, then the point-x function reads what point is assigned to x of the point.
